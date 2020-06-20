d1f1e05c62dab2075c01db0ef9c5b818efe1bb1a
liuzhao <liuzhao@qianmi.com>
2020-06-20 17:34:58 +0800

    index.tsx可以打包成index.js了

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 module.exports = {
+  mode: 'production',
   entry: {
     index: './lib/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Lui',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
