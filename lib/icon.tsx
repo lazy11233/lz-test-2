9a8f36a4a23e82d0dbbdb1749907adf1eb95749e
liuzhao <liuzhao@qianmi.com>
2020-07-05 13:25:39 +0800

    添加icon的点击事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -5,11 +5,12 @@ import './icon.scss';
 
 interface IconProps {
   name: string;
+  onClick: () => void;
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <svg className="lui-icon">
+    <svg className="lui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`} />
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 
 import Icon from './icon';
 
+const fn = () => {
+  console.log('fn');
+}
+
 ReactDOM.render((
   <div>
-    <Icon name="wechat" />
-    <Icon name="alipay" />
+    <Icon name="wechat" onClick={fn} />
   </div>), document.getElementById('root'));
