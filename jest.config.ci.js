721022fd31dac8a0fe66ee5459ca5d234ef678bb
liuzhao <liuzhao@qianmi.com>
2020-07-14 19:39:49 +0800

    add ci config

---
+++ jest.config.ci.js
@@ -0,0 +1,9 @@
+const base = require('./jest.config');
+
+module.exports = Object.assign({}, base, {
+  collectCoverage: true,
+  collectCoverageFrom: ['{lib,include}/**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  reporters: ['default', 'jest-junit']
+})
--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
-  collectCoverage: true,
-  collectCoverageFrom: ['{lib,include}/**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-  reporters: ['default', 'jest-junit'],
+  collectCoverage: false,
+
+  reporters: ['default'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules', 'include'],
   moduleNameMapper: {
