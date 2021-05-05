a12f4261eed460c6ebff0fc137d354d2a1bdf502
lazy11233 <1257625996@qq.com>
2021-05-05 18:05:01 +0800

    [feat]表单支持自定义校验和异步校验

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -3,26 +3,50 @@ import Form, { FormValue } from './form';
 import Validator from './validate';
 import Button from '../button/button';
 
+const checkUserName = (
+  username: string,
+  success: () => void,
+  fail: () => void
+) => {
+  const exceedNames = ['frank', 'jack', 'alice', 'bob '];
+  setTimeout(() => {
+    if (exceedNames.includes(username)) {
+      success();
+    } else {
+      fail();
+    }
+  }, 3000);
+};
+
 const FormExample: React.FunctionComponent = () => {
   const [formData, setFormData] = useState<FormValue>({
     username: '',
     password: '',
   });
   const [fields] = useState([
-    { name: 'username', label: '用户名', input: { type: 'text' } },
+    { name: 'username', label: '用户名', input: { type: 'text' }},
     { name: 'password', label: '密码', input: { type: 'password' } },
   ]);
   const [errors, setErrors] = useState({});
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     const rules = [
-      { key: 'username', required: true },
-      { key: 'username', min: 3, max: 10 },
-      { key: 'username', pattern: /^[A-Za-z0-9]+$/ },
-      { key: 'password', required: true },
+      { key: 'username', required: true, message: '用户名不能为空' },
+      { key: 'username', min: 3, max: 10, message: '用户名长度为3~10位' },
+      { key: 'username', pattern: /^[A-Za-z0-9]+$/, message: '只能输入数字和英文字母' },
+      {
+        key: 'username',
+        validate: (username: string) => {
+          return new Promise<void>((resolve, reject) => {
+            checkUserName(username, resolve, reject);
+          });
+        },
+        message: '用户名唯一'
+      },
+      { key: 'password', required: true, message: '密码不能为空' },
     ];
-    const errors = Validator(formData, rules);
-    setErrors(errors);
-    console.log(errors);
+    Validator(formData, rules, (errors) => {
+      setErrors(errors)
+    });
   };
 
   return (
@@ -41,9 +65,7 @@ const FormExample: React.FunctionComponent = () => {
             <Button type="reset" level="dangerous">
               取消
             </Button>
-            <Button>
-              普通
-            </Button>
+            <Button>普通</Button>
           </Fragment>
         }
         onChange={setFormData}
--- lib/form/validate.tsx
+++ lib/form/validate.tsx
@@ -3,45 +3,99 @@ import { FormValue } from './form';
 interface FormRule {
   key: string;
   required?: boolean;
+  message: string;
   min?: number;
   max?: number;
   pattern?: RegExp;
+  validate?: (value: any) => Promise<void>;
 }
 
 type FormRules = Array<FormRule>;
 
-interface FormErrors {
-  [key: string]: string[];
-}
+type OneError = {
+  message: string;
+  promise?: Promise<any>;
+};
 
 const isEmpty = (value: any): boolean => {
   return value === '' || value === null || value === undefined;
 };
 
-const Validator = (formValue: FormValue, rules: FormRules): FormErrors => {
+const Validator = (
+  formValue: FormValue,
+  rules: FormRules,
+  callback: (errors: any) => void
+): void => {
   let errors: any = {};
-  const addError = (key: string, message: string) => {
+  const addError = (key: string, error: OneError) => {
     if (errors[key] === undefined) {
       errors[key] = [];
     }
-    errors[key].push(message);
+    errors[key].push(error);
   };
-  rules.map((rule) => {
+  for (let i = 0; i < rules.length; i++) {
+    let rule = rules[i];
     const value = formValue[rule.key];
+    if (rule.validate) {
+      // 自定义校验器
+      const promise = rule.validate(value);
+      addError(rule.key, { message: rule.message, promise });
+    }
     if (rule.required && isEmpty(value)) {
-      addError(rule.key, '必填');
+      addError(rule.key, { message: rule.message });
     }
     if (rule.min && !isEmpty(value) && value.length < rule.min) {
-      addError(rule.key, '太短');
+      addError(rule.key, { message: rule.message });
     }
     if (rule.max && !isEmpty(value) && value.length > rule.max) {
-      addError(rule.key, '太长');
+      addError(rule.key, { message: rule.message });
     }
     if (!isEmpty(value) && rule.pattern && !rule.pattern.test(value)) {
-      addError(rule.key, '格式不正确');
+      addError(rule.key, { message: rule.message });
     }
-  });
-  return errors;
+  }
+  Promise.all(
+    flat(Object.values(errors))
+      .filter((item) => item.promise)
+      .map((item) => item.promise)
+  ).then(
+    () => {
+      const newErrors: [string, string[]] = Object.keys(errors).map((key) => [
+        key,
+        errors[key].map((item: OneError) => item.message),
+      ]);
+      fromEntries(newErrors);
+      callback(fromEntries(newErrors));
+    },
+    () => {
+      const newErrors: [string, string[]] = Object.keys(errors).map((key) => [
+        key,
+        errors[key].map((item: OneError) => item.message),
+      ]);
+      fromEntries(newErrors);
+      callback(fromEntries(newErrors));
+    }
+  );
+};
+
+const flat = (array: Array<any>) => {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] instanceof Array) {
+      result.push(...array[i]);
+    } else result.push(array[i]);
+  }
+  return result;
+};
+
+const fromEntries = (array: [string, string[]]) => {
+  console.log('我是一个兵', array);
+  let result: {[key: string]: string[]} = {};
+  for (let i = 0; i < array.length; i++) {
+
+    result[array[i][0]] = array[i][1];
+  }
+  return result;
 };
 
 export default Validator;
